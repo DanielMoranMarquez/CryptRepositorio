ecc7fffead65c18c150a1dd015dd6bf4daba1c43
Daniel Morán <daniel.moranmarquez@telefonica.com>
2023-04-21 14:02:01 +0200

    +

--- Script/Hidding.swift
+++ Script/Hidding.swift
@@ -7,7 +7,33 @@
 
 import Foundation
 
-public struct Hidding {
-    var secreto = "secreto"
+@main
+enum Hidding {
+    static func main() {
+        //1
+        guard let infoFile = ProcessInfo.processInfo
+          .environment["INFOPLIST_FILE"]
+        else {
+          return
+        }
+        guard let projectDir = ProcessInfo.processInfo.environment["SRCROOT"] else {
+          return
+        }
+
+        //2
+        if var dict = NSDictionary(contentsOfFile:
+          projectDir + "/" + infoFile) as? [String: Any] {
+          guard let _ = dict["Secreto"] as? String else { return }
+            
+          // 3
+            dict["Secreto"] = "tercero"
+          // 4
+          
+          // 5
+          (dict as NSDictionary).write(
+            toFile: projectDir + "/" + infoFile,
+            atomically: true)
+        }
+    }
 }
 
