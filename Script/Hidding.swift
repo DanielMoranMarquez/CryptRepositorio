9b0998ebd22d9a3f3d9bcd37481de9ce821b23de
Daniel Morán <daniel.moranmarquez@telefonica.com>
2023-04-21 10:02:08 +0200

    +

---
+++ Script/Hidding.swift
@@ -0,0 +1,13 @@
+//
+//  Hidding.swift
+//  Crypt
+//
+//  Created by Daniel Morán on 21/4/23.
+//
+
+import Foundation
+
+public struct Hidding {
+    var secreto = "secreto"
+}
+
---
+++ Secrets2/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  Crypt
+//
+//  Created by Daniel Morán on 20/4/23.
+//
+
+import Foundation
+
+public static struct Secret {
+    var consumerSecret = "Alfredo"
+}
